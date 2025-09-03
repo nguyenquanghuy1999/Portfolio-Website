241657e3ca0ce2047822f20a54ccf418ca6e85e2
nguyenquanghuy1999 <nguyenquanghuy1999>
2025-09-03 15:49:31 +0700

    feat: xử lý click logo hiện thị loading screen

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-const loadingScreen = document.querySelector("#loading-screen")
+const loadingScreen = document.querySelector("#loading-screen");
 
-setTimeout(() => {
-    loadingScreen.style.display = "none";
 
-}, 2000);
+function handleLoadingScreen() {
+    setTimeout(() => {
+        loadingScreen.style.display = "none";
+    }, 2000);
+}
+handleLoadingScreen();
+
+
+const logoElement = document.querySelector(".hexagon.is-hover");
+if (logoElement) {
+    logoElement.onclick = () => {
+        loadingScreen.style.display = "flex";
+        loadingScreen.innerHTML = `
+         <div id="logo">
+            <svg class="hexagon" width="130" height="130" viewBox="0 0 360 280"
+                xmlns="http://www.w3.org/2000/svg">
+                <polygon points="150,15 42,77 42,202 150,265 258,202 258,77" fill="none" stroke="#64FFDA"
+                    stroke-width="13" stroke-dasharray="750" stroke-dashoffset="750">
+                    <animate attributeName="stroke-dashoffset" from="750" to="0" dur="1s" fill="freeze" />
+                </polygon>
+                <text x="110" y="175" fill="#64FFDA" font-size="110" opacity="0">
+                    B
+                    <animate attributeName="opacity" from="0" to="1" begin="1s" dur="0.7s" fill="freeze" />
+                </text>
+            </svg>
+        </div>
+        `
+        handleLoadingScreen();
+    }
+}
+
