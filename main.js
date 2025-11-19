0cff949d687d3686f90e699ec7950a0a7675a597
nguyenquanghuy1999 <nguyenquanghuy1999>
2025-11-19 17:00:40 +0700

    refactor: thay đổi style + cấu trúc code k ảnh hưởng UI và Logic

--- main.js
+++ main.js
@@ -32,52 +32,11 @@ function hideLoadingScreen() {
     }, 2000);
 }
 
-function showLoadingScreen() {
-    loadingScreen.style.display = "flex";
-}
-function handleResetSidebar() {
-    navListEl.classList.remove("show");
-    closeIcon.style.display = "none";
-
-    //tablet and mobile
-    if (window.screen.width <= 1023) {
-        menuIconEl.style.display = "block";
-    }
-
-}
 
 function handleClickLogo() {
     const logoElement = document.querySelector(".hexagon.is-hover");
     if (logoElement) {
-        logoElement.addEventListener("click", () => {
-
-            handleResetSidebar();
-
-            showLoadingScreen();
-            loadingScreen.innerHTML = `
-            <div id="logo">
-                <svg class="hexagon" width="130" height="130" viewBox="0 0 360 280" xmlns="http://www.w3.org/2000/svg">
-                    <polygon points="150,15 42,77 42,202 150,265 258,202 258,77" fill="none" stroke="#64FFDA"
-                        stroke-width="13" stroke-dasharray="750" stroke-dashoffset="750">
-                        <animate attributeName="stroke-dashoffset" from="750" to="0" dur="1s" fill="freeze" />
-                    </polygon>
-                    <text x="110" y="175" fill="#64FFDA" font-size="110" opacity="0">
-                        B
-                        <animate attributeName="opacity" from="0" to="1" begin="1s" dur="0.7s" fill="freeze" />
-                    </text>
-                </svg>
-            </div>
-            `
-            hideLoadingScreen();
-
-            // chạy lại animate nav items
-            handleAnimateNavItems();
-            // chạy lại animate hero items
-            handleAnimateHeroItems();
-
-            scrollToTop();
-
-        })
+        logoElement.addEventListener("click", () => location.reload()); 
     }
 }
 handleClickLogo();
@@ -174,6 +133,7 @@ function handleScrollAnimate() {
     document.querySelectorAll("section, .project-item").forEach((item) => observer.observe(item));
 }
 handleScrollAnimate();
+
 
 function handleTabListJobs() {
     const data = [
@@ -335,4 +295,4 @@ function handleProjectOtherItemMouseLeave() {
     }
 }
 
-handleProjectOtherItemMouseLeave()
+handleProjectOtherItemMouseLeave(); 
